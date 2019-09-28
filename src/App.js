bc74723cd82d399474d374a38c20fd4de8e5a7cc
Piotr Napieralski <piotrn-87@o2.pl>
2019-09-28 17:49:29 +0200

    Feat: Added Header style

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./Header";
+import MemeGenerator from "./MemeGenrator";
+import style from "./style.css";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <MemeGenerator />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/MemeGenrator.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class MemeGenreator extends React.Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+  render() {
+    return <h1>Meme Generator</h1>;
+  }
+}
+
+export default MemeGenreator;
