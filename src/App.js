e8397dad1d4ea7d825b2c3794abc89e51a9a647a
Piotr Napieralski <piotrn-87@o2.pl>
2019-10-07 21:40:56 +0200

    Fix: Added a Sass stylesheet

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import Header from "./Header";
 import MemeGenerator from "./MemeGenerator";
 import Footer from "./Footer";
-import "./style.css";
+import "./style.scss";
 
 function App() {
   return (
