e0ccfdf7f366d9fcb54f88b8ed3295332cd2b9b5
Piotr Napieralski <piotrn-87@o2.pl>
2019-10-07 21:29:03 +0200

    Feat: Added footer to meme-generator

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "./Header";
 import MemeGenerator from "./MemeGenerator";
+import Footer from "./Footer";
 import "./style.css";
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <div>
       <Header />
       <MemeGenerator />
+      <Footer />
     </div>
   );
 }
---
+++ src/Footer.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Footer() {
+  return <p className="footer">Made with ❤ by human form Boat City, Earth </p>;
+}
+
+export default Footer;
