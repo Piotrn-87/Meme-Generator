f9770f50ca01b1d4ce391dbd85a19dcc5c2e0221
Piotr Napieralski <piotrn-87@o2.pl>
2019-10-19 19:12:57 +0200

    Updates

---
+++ precache-manifest.c2f4793696e6b79954d012145e1557d2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eddfd432c0a1e0a769e1f5cb2fc4b620",
+    "url": "/Meme-Generator/index.html"
+  },
+  {
+    "revision": "9586801b074cbc330cce",
+    "url": "/Meme-Generator/static/css/main.c5d1db15.chunk.css"
+  },
+  {
+    "revision": "2e801cc69383f418069e",
+    "url": "/Meme-Generator/static/js/2.c111f117.chunk.js"
+  },
+  {
+    "revision": "9586801b074cbc330cce",
+    "url": "/Meme-Generator/static/js/main.10f9b815.chunk.js"
+  },
+  {
+    "revision": "2977d8f05cd9e777b483",
+    "url": "/Meme-Generator/static/js/runtime-main.fd067ed6.js"
+  }
+]);
