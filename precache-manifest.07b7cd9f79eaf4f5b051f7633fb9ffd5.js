1e2b202ec59c4cd0e6135a23d0352d59e79394b9
Piotr Napieralski <piotrn-87@o2.pl>
2019-10-30 19:10:45 +0100

    Updates

--- precache-manifest.07b7cd9f79eaf4f5b051f7633fb9ffd5.js
+++ precache-manifest.07b7cd9f79eaf4f5b051f7633fb9ffd5.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "1e1c504503f7cae8c51a3d5a8e1022d9",
+    "revision": "bf740ebc88845fd54661a56725ff32e0",
     "url": "/Meme-Generator/index.html"
   },
   {
-    "revision": "688cd7e3cd4ff588c010",
-    "url": "/Meme-Generator/static/css/main.be518d15.chunk.css"
+    "revision": "59cc53df06c82253b739",
+    "url": "/Meme-Generator/static/css/main.aa13937f.chunk.css"
   },
   {
     "revision": "2e801cc69383f418069e",
     "url": "/Meme-Generator/static/js/2.c111f117.chunk.js"
   },
   {
-    "revision": "688cd7e3cd4ff588c010",
-    "url": "/Meme-Generator/static/js/main.10f9b815.chunk.js"
+    "revision": "59cc53df06c82253b739",
+    "url": "/Meme-Generator/static/js/main.61619946.chunk.js"
   },
   {
     "revision": "2977d8f05cd9e777b483",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Meme-Generator/precache-manifest.2e546aa02c6e36a7650fd60effef7dcd.js"
+  "/Meme-Generator/precache-manifest.07b7cd9f79eaf4f5b051f7633fb9ffd5.js"
 );
 
 self.addEventListener('message', (event) => {
